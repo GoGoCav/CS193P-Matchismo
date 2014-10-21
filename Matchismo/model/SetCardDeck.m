48082fd890cd5fdcc6b6f775ad68d46f3cb92e2c
Canvas Hsu <gogocav@gmail.com>
2014-10-21 19:00:05 +0800

    assignment 3 required task 2

--- Matchismo/model/SetCardDeck.m
+++ Matchismo/model/SetCardDeck.m
@@ -18,6 +18,8 @@
     if (self) {
         SetCard *card = [[SetCard alloc] init];
         
+        int i = 1;
+        
         for (NSString *symbol in [SetCard validSymbol]) {
             for (NSString *color in [SetCard validColor]) {
                 for (NSString *shading in [SetCard validShading]) {
@@ -26,6 +28,7 @@
                         card.color = color;
                         card.shading = shading;
                         card.number = number;
+                        NSLog(@"%@-%@-%@-%ld:%d",card.symbol,card.color,card.shading,card.number,i++);
                         [self addCard:card];
                     }
                 }
